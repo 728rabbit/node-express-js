3cce5cd70ef831e9ae33db1dad8fe2ac6553c9fc
Kaiyun <kaiyunchan2012@hotmail.com>
2026-05-13 12:35:52 +0800

    Update db.js

--- mysql/db.js
+++ mysql/db.js
@@ -16,4 +16,24 @@ function db_query(tableName) {
     return new QueryBuilder(tableName, pool);
 }
 
-module.exports = { pool, db_query };
+async function db_row_query(sql, params = []) {
+    const [rows] = await pool.query(sql, params);
+    return rows;
+}
+
+async function db_transaction(callback) {
+    const connection = await pool.getConnection();
+    await connection.beginTransaction();
+    try {
+        const result = await callback(connection);
+        await connection.commit();
+        return result;
+    } catch (error) {
+        await connection.rollback();
+        throw error;
+    } finally {
+        connection.release();
+    }
+}
+
+module.exports = { pool, db_query, db_row_query, db_transaction};
